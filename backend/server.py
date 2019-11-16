2326a4fb185314be7a38e46c90719b8c649a2e24
vonion <tsybulkovitaliy@gmail.com>
2019-11-16 17:25:28 +0200

    naive version of chat

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,19 @@
-from bottle import route, run, static_file, get
+from bottle import route, run, static_file, get, post, request
 from dotenv import load_dotenv
+
+from backend.services import chat
 
 
 @route("/")
 def index():
     return server_static("index.html")
+
+
+@post("/chat")
+def handle_input():
+    input = request.forms.get("input")
+    case_tag = request.forms.get("case_tag")
+    return chat(case_tag, input)
 
 
 @get("/usually-returned/<filepath:path>")
@@ -13,6 +22,5 @@ def server_static(filepath):
 
 
 if __name__ == '__main__':
-    # TEMPLATE_PATH.append("../web/index_files")
     load_dotenv()
     run(host="localhost", port=8080, debug=True)
--- backend/services.py
+++ backend/services.py
@@ -1,2 +1,13 @@
-def get_some_stuff():
-    return {"hello": "world"}
+dialog_options = {
+    "ok, removing":
+        ["yes", "yep", "sure", "let's do it", "lets do it", "why not"],
+    "if you say so":
+        ["no", "nope", "abort", "cancel"]
+}
+
+
+def chat(case_tag, input):
+    lowercase = input.lower()
+    for answer in dialog_options:
+        if lowercase in dialog_options[answer]:
+            return answer

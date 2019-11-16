1fa88ed1f59017a4e4e68bb3d4a4860a4679a66f
Albert221 <w.albert221@gmail.com>
2019-11-16 22:45:44 +0100

    Fix size website images

--- web/static/wrong-price/whatever.js
+++ web/static/wrong-price/whatever.js
@@ -1,79 +1,5 @@
 const API_URL = 'https://junction-retail.herokuapp.com';
 
-let currentColor = 0;
-let selectedSize;
-
-const sizesSelect = document.querySelector('#size-select');
-const colorsList = document.querySelector('#color-select');
-
-(function () {
-    const sizes = [
-        [5, 6, 7, 8],
-        [5, 7, 9]
-    ];
-
-    const images = [
-        'https://images-na.ssl-images-amazon.com/images/I/61-o2BCVzgL._SY500._SX._UX._SY._UY_.jpg',
-        'https://images-na.ssl-images-amazon.com/images/I/81AoRZGmqsL._SX500._SX._UX._SY._UY_.jpg'
-    ];
-
-    const thumbnailImg = document.querySelector('.item.imageThumbnail img');
-    const bigImg = document.querySelector('.item.image img');
-
-    colorsList.addEventListener('click', e => {
-        if (e.target.tagName.toLowerCase() != 'img') {
-            return;
-        }
-        
-        // 9th ascendant
-        const liElement = e.target.parentNode.parentNode.parentNode
-            .parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
-        
-        const oldIndex = currentColor;
-        const index = [...colorsList.children].indexOf(liElement);
-        currentColor = index;
-
-        if (!sizes[index].some(el => el == selectedSize)) {
-            const sizeIndex = sizes[oldIndex].indexOf(selectedSize);
-            if (sizeIndex !== -1) {
-                console.warn('lol');
-                // Fallback to other size when there is no such size in that color.
-                selectedSize = sizes[index][sizeIndex-1] || sizes[index][sizeIndex+1] || sizes[index][0];
-            }
-        }
-
-        render();
-    });
-
-    sizesSelect.addEventListener('input', e => {
-        selectedSize = e.target.value;
-    });
-
-    const render = () => {
-        // Render sizes
-        sizesSelect.innerHTML = '<option>Select</option>';
-        sizes[currentColor].forEach(size => {
-            sizesSelect.insertAdjacentHTML('beforeend', `
-                <option value="${size}" ${size == selectedSize ? 'selected' : ''}>${size}</option>
-            `)
-        });
-
-        thumbnailImg.src = images[currentColor];
-        bigImg.src = images[currentColor];
-
-        // Render selected color
-        [...colorsList.children].forEach((child, i) => {
-            if (currentColor == i) {
-                child.classList.add('swatchSelect');
-            } else {
-                child.classList.remove('swatchSelect');
-            }
-        });
-    };
-
-    render();
-})();
-
 const addToBasketBtn = document.querySelector('#add-to-basket');
 
 const chatbotBox = document.querySelector('#chatbot');
@@ -99,9 +25,9 @@ const sendChat = (message) => {
     }
 
     const data = new FormData();
-    data.append('case_tag', message == '' ? 'size' : '');
+    data.append('case_tag', message == '' ? 'price' : '');
     data.append('input', message);
-    data.append('size', parseFloat(selectedSize));
+    data.append('size', 0.0);
 
     fetch(API_URL + '/chat', {
         method: 'POST',

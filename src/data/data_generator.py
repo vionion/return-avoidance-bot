30931c1e3f3211b1457eb47255f75c9898f4e16f
vonion <tsybulkovitaliy@gmail.com>
2019-11-17 09:23:32 +0200

    fix shoe size

--- src/data/data_generator.py
+++ src/data/data_generator.py
@@ -98,8 +98,7 @@ def _gen_purchase_data(conn: Connection):
     conn.commit()
 
     demo_cus_name = "demo_customer"
-    demo_prod_size = choice([5, 7, 9])
-    print(demo_prod_size)
+    demo_prod_size = 7
     demo_prod_cat = "shoes"
 
     date_strs = [
